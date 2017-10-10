8a5cd396fb74190fb989f80c11f5c5ca42d67146
zhangyuxuan <zhangyuxuan1024@163.com>
2017-10-10 13:50:22 +0800

    在MainActivity里新添加了一个按钮

--- app/src/main/java/com/fw/demo/MainActivity.java
+++ app/src/main/java/com/fw/demo/MainActivity.java
@@ -1,13 +1,34 @@
 package com.fw.demo;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn1;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn1:
+                Toast.makeText(context,"点击",Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
